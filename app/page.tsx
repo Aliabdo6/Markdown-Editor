560ab3012931d811480579acf40f0b6a54986cdb
Aliabdo6 <aliellool202020@gmail.com>
2025-04-02 06:19:57 +0200

    up

--- app/page.tsx
+++ app/page.tsx
@@ -169,18 +169,3 @@ export default function Home() {
     </main>
   );
 }
-
-// import MarkdownEditor from "./components/MarkdownEditor";
-
-// export default function Home() {
-//   return (
-//     <main className="min-h-screen bg-white">
-//       <div className="container mx-auto py-8">
-//         <h1 className="text-3xl font-bold mb-8 text-center">
-//           Markdown Editor
-//         </h1>
-//         <MarkdownEditor />
-//       </div>
-//     </main>
-//   );
-// }
